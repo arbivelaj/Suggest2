94f080bc9defb2e8f77c0db5c46accce925dc438
arbi velaj <arbi.velaj@gmail.com>
2017-09-24 17:58:30 +0200

    valitdations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,16 @@
 class Review < ApplicationRecord
   belongs_to :user
+  CATEGORIES = %w(restaurant bar shop place)
+
+  validates :title, presence: true
+  validates :desription, presence: true
+  validates :item, presence: true
+  validates :place, presence: true
+
+  validates :location, presence: true
+  validates :desription, length: { minimum: 20 }
+  validates :category, inclusion: { in: CATEGORIES }
+  validates :rating_spot, presence: true, numericality: {only_integer: true}, inclusion: {in: [0,1,2,3,4,5], allow_nil: false }
+
+
 end
