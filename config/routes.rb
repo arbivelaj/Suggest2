16df35f705d6d0c488459d8fc7f2b0962c8dda4e
arbi velaj <arbi.velaj@gmail.com>
2017-09-24 20:17:17 +0200

    controller beginning

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,43 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
+  get 'profiles/new'
+
+  get 'profiles/create'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
+  get 'comments/index'
+
+  get 'comments/show'
+
+  get 'comments/new'
+
+  get 'comments/update'
+
+  get 'comments/create'
+
+  get 'comments/edit'
+
+  get 'reviews/index'
+
+  get 'reviews/show'
+
+  get 'reviews/new'
+
+  get 'reviews/update'
+
+  get 'reviews/edit'
+
   devise_for :users
   root to: 'pages#home'
+
+  resources :reviews do
+  resources :comments, only: [:new, :create]
+end
+
+  resources :profiles, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
